026804c0b358d3abb38147b7c3747c7bca0c3c1d
Jagepard <dankorot@gmail.com>
2018-08-30 17:25:18 +0300

    Add file

---
+++ src/InterpreterInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @license   https://mit-license.org/ MIT
+ */
+
+namespace Behavioral\Interpreter;
+
+/**
+ * Interface InterpreterInterface
+ * @package Behavioral\Interpreter
+ */
+interface InterpreterInterface
+{
+
+    /**
+     * @param string $input
+     */
+    public function interpret(string $input): void;
+}
