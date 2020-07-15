b6fb4306794b87b9891febd12e3a548e58c284b7
Jagepard <dankorot@gmail.com>
2020-07-15 10:51:40 +0300

    adds class

---
+++ src/RegistryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
+namespace Behavioral\Interpreter;
+
+interface RegistryInterface
+{
+    /**
+     * @return array
+     */
+    public function getData(): array;
+}
